231d663f95a33856367d62b0c8c57977024a6991
Janviermbilizi <janviermbilizi@gmail.com>
2023-12-17 05:04:48 -0500

    Removed unecessary borders & spaces

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -88,17 +88,19 @@ function Home() {
                   style={{ width: "100%", height: "100%" }}
                 />
               </div>
-              <div className="col-md-8 text-start">
+              <div className="col-md-8 text-md-start">
                 <div className="card-body">
                   <h1 className="card-title">Mary Formeller, Director</h1>
+                  <br></br>
                   <p className="card-text fs-5">
                     Retired Montgomery County Public Schools (MCPS) Special
                     Education Teacher with 40+ years serving young adults with
                     varying needs.
                   </p>
                   <p className="card-text fs-5">
-                    MD state Teaching Certification
+                    MD State Teaching Certification
                   </p>
+                  <br></br>
                   <p className="card-text fs-5">MKFormeller@gmail.com</p>
                   <p className="card-text fs-5">301-633-6834</p>
                   <a
@@ -149,10 +151,10 @@ function Home() {
           </ModalLink> */}
         </section>
 
-        <section id="contact">
+        <section id="contact" className="mb-4">
           <div className="card">
             <div className="row align-items-center g-0 px-2">
-              <div className="col-md-4 border-end p-0">
+              <div className="col-md-4 border-at-end p-0">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="200"
@@ -165,10 +167,32 @@ function Home() {
                   <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2" />
                 </svg>
               </div>
-              <div className="col-md-8 text-start">
+              <div className="col-md-8 text-md-start">
                 <div className="card-body">
                   <Contact showTitle={false} />
                 </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="testimonials">
+          <div className="card">
+            <div className="row align-items-center g-0 px-2">
+              <div className="col-md-4 p-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="200"
+                  height="200"
+                  fill="currentColor"
+                  class="bi bi-people"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                </svg>
+              </div>
+              <div className="col-md-8 text-md-start ps-4">
+                <p className="fs-3">Testimonials are coming soon...</p>
               </div>
             </div>
           </div>

54480c8243e5f888ad4471394857ad20b1c93f34
Janviermbilizi <janviermbilizi@gmail.com>
2024-02-05 18:58:59 -0500

    Added testimonials

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -8,9 +8,30 @@ function Home() {
 
   const currentYear = new Date().getFullYear();
 
+  const testimonials = [
+    {
+      text: "Mrs. Formeller, our remarkable team member, has consistently showcased her exceptional talents in effectively collaborating with both me and the other team members of That Smoothie Place. What sets her apart is her proficiency in working with team members who have developmental disabilities. She displays a remarkable understanding and empathy towards their unique needs and takes great pride in ensuring they have a positive and inclusive work environment. Mrs. Formeller's dedication and ability to foster a sense of belonging within our team truly makes her an invaluable asset to our smoothie family.",
+      author: "Mrs. Monique Sotomarino",
+      jobTitle: "Owner",
+      business: "That Smoothie Place",
+    },
+    {
+      text: "Ms. Formeller was a critical member of our staff and a leader, building positive relationships with students and staff. She implements new and creative ways to reach all learners and ensure that every student succeeds. As our special education transition specialist, Ms. Formeller displayed understanding and empathy towards all of our students’ unique needs. She was a champion of ensuring that all students had an opportunity to work in an inclusive work environment.As the transition specialist, Ms. Formeller was an outstanding advocate for all our students, ensuring that each student and family had a plan for success as they exited our discreet programs at DHS.",
+      author: "Mr. Kevin Yates",
+      jobTitle: "Principal",
+      business: "Damascus High School",
+    },
+    {
+      text: "Throughout her time at Damascus High School, she consistently demonstrated qualities that helped our Damascus community. One of her greatest strengths is her ability to work well with families. She showed a remarkable talent for building strong and meaningful relationships with parents and caregivers, ensuring open lines of communication and fostering a collaborative approach to education. Her dedication to involving families in the educational process has been invaluable, instilling trust and confidence in our school community.",
+      author: "Mr. Christopher Ohm",
+      jobTitle: "Resource Teacher in Special Education",
+      business: "Damascus HS",
+    },
+  ];
+
   return (
     <div className="App">
-      <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <h1
             className="text-white"
@@ -112,7 +133,7 @@ function Home() {
                       width="40"
                       height="50"
                       fill="currentColor"
-                      class="bi bi-facebook"
+                      className="bi bi-facebook"
                       viewBox="0 0 16 16"
                     >
                       <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
@@ -160,7 +181,7 @@ function Home() {
                   width="200"
                   height="200"
                   fill="currentColor"
-                  class="bi bi-chat-dots"
+                  className="bi bi-chat-dots"
                   viewBox="0 0 16 16"
                 >
                   <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
@@ -177,31 +198,48 @@ function Home() {
         </section>
 
         <section id="testimonials">
-          <div className="card">
-            <div className="row align-items-center g-0 px-2">
-              <div className="col-md-4 p-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="200"
-                  height="200"
-                  fill="currentColor"
-                  class="bi bi-people"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-                </svg>
-              </div>
-              <div className="col-md-8 text-md-start ps-4">
-                <p className="fs-3">Testimonials are coming soon...</p>
-              </div>
+          <div className="container jumbotron rounded border">
+            <div className="row py-2">
+              <h1>Testimonials</h1>
             </div>
+
+            {testimonials.map((testimonial) => (
+              <div key={testimonial.author} className="row">
+                <div className="col-12">
+                  <div className="row border-top m-2"></div>
+                </div>
+                <div className="col-12">
+                  <div className="row">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="150"
+                      height="150"
+                      fill="currentColor"
+                      className="bi bi-person col-md-auto"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                    </svg>
+                    <div className="col">
+                      <div className="card-body text-md-start">
+                        <p className="card-text fs-6">{testimonial.text}</p>
+                        <p className="card-text fs-6 fw-bold">
+                          - {testimonial.author}, {testimonial.jobTitle},{" "}
+                          {testimonial.business}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       </main>
       <footer className="p-4">
         <p>&copy; MKFormeller, LLC {currentYear}. All rights reserved.</p>
         <span>
-          <a href="#home">Privacy Policy </a>-
+          <a href="#home">Privacy Policy</a> -{" "}
           <a href="#home">Terms of Service</a>
         </span>
       </footer>

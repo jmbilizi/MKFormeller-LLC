63ce47a7c62925f5f1f122fbd17fd5afccfc0b63
Janviermbilizi <janviermbilizi@gmail.com>
2024-04-23 21:18:04 -0400

    Updated provider info

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -53,17 +53,17 @@ function Home() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
-              <a className="nav-link" aria-current="page" href="#about">
-                Abouts
-              </a>
               <a className="nav-link" href="#services">
                 Services
               </a>
-              <a className="nav-link" href="#contact">
-                Contact
+              <a className="nav-link" aria-current="page" href="#provider">
+                Provider
               </a>
               <a className="nav-link" href="#testimonials">
                 Testimonials
+              </a>
+              <a className="nav-link" href="#contact">
+                Contact
               </a>
             </div>
           </div>
@@ -99,7 +99,7 @@ function Home() {
           </div>
         </section>
 
-        <section id="about" className="mb-4">
+        <section id="provider" className="mb-4">
           <div className="card mb-3">
             <div className="row g-0 px-2">
               <div className="col-md-4 p-4">
@@ -118,12 +118,14 @@ function Home() {
                     Education Teacher with 40+ years serving young adults with
                     varying needs.
                   </p>
-                  <p className="card-text fs-5">
-                    MD State Teaching Certification
+                  <p className="card-text fs-5 bold">
+                    <strong>* MD State Teaching Certification</strong>
                   </p>
-                  <br></br>
-                  <p className="card-text fs-5">MKFormeller@gmail.com</p>
-                  <p className="card-text fs-5">301-633-6834</p>
+                  <p className="card-text fs-5 bold">
+                    <strong> * Maryland DDA Certified Provider </strong>
+                  </p>
+                  <p className="card-text fs-5 lead">MKFormeller@gmail.com</p>
+                  <p className="card-text fs-5 lead">301-633-6834</p>
                   <a
                     href="https://www.facebook.com/mary.formeller"
                     className="card-text fs-5"
@@ -172,31 +174,6 @@ function Home() {
           </ModalLink> */}
         </section>
 
-        <section id="contact" className="mb-4">
-          <div className="card">
-            <div className="row align-items-center g-0 px-2">
-              <div className="col-md-4 border-at-end p-0">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="200"
-                  height="200"
-                  fill="currentColor"
-                  className="bi bi-chat-dots"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
-                  <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2" />
-                </svg>
-              </div>
-              <div className="col-md-8 text-md-start">
-                <div className="card-body">
-                  <Contact showTitle={false} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <section id="testimonials">
           <div className="container jumbotron rounded border">
             <div className="row py-2">
@@ -233,6 +210,31 @@ function Home() {
                 </div>
               </div>
             ))}
+          </div>
+        </section>
+
+        <section id="contact" className="mb-4">
+          <div className="card">
+            <div className="row align-items-center g-0 px-2">
+              <div className="col-md-4 border-at-end p-0">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="200"
+                  height="200"
+                  fill="currentColor"
+                  className="bi bi-chat-dots"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+                  <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2" />
+                </svg>
+              </div>
+              <div className="col-md-8 text-md-start">
+                <div className="card-body">
+                  <Contact showTitle={false} />
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
